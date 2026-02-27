f3bfb653689f9295bcf14debc4fda697786a7eff
vijaykrishan-itmanager <er.vijay.krishan9@outlook.com>
2026-02-27 22:21:09 +0530

    Create connect-m365.ps1

---
+++ connect-m365.ps1
@@ -0,0 +1,16 @@
+# Connect to Microsoft Graph
+
+Write-Host "Connecting to Microsoft Graph..." -ForegroundColor Cyan
+
+# Install module if not available
+if (-not (Get-Module -ListAvailable -Name Microsoft.Graph)) {
+    Install-Module Microsoft.Graph -Scope CurrentUser -Force
+}
+
+Import-Module Microsoft.Graph
+
+Connect-MgGraph -Scopes "User.Read.All","Directory.Read.All"
+
+Write-Host "Connected Successfully!" -ForegroundColor Green
+
+Get-MgUser -Top 10 | Select-Object DisplayName, UserPrincipalName
