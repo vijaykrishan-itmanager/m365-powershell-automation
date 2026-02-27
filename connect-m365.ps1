3f7b2d1e9a47b87048db9a1d26ca650d3a8520b7
vijaykrishan-itmanager <er.vijay.krishan9@outlook.com>
2026-02-27 22:25:07 +0530

    Added TODO tasks for automation expansion

--- connect-m365.ps1
+++ connect-m365.ps1
@@ -14,3 +14,6 @@ Connect-MgGraph -Scopes "User.Read.All","Directory.Read.All"
 Write-Host "Connected Successfully!" -ForegroundColor Green
 
 Get-MgUser -Top 10 | Select-Object DisplayName, UserPrincipalName
+# TODO:
+# Add license reporting automation
+# Add admin role export script
